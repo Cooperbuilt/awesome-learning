6eb73337b31dcac57e0bb52f5566de25dd87e606
Evan Cooper <evcooper@wayfair.com>
2019-05-03 10:37:33 -0400

    scss module conversion - Post

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -4,7 +4,7 @@ import Author from './Author';
 import Comments from './Comments';
 import Content from './Content';
 import Tags from './Tags';
-import styles from './Post.module.scss';
+import './Post.scss';
 
 const Post = ({ post }) => {
   const {
@@ -17,19 +17,19 @@ const Post = ({ post }) => {
   const { tagSlugs } = post.fields;
 
   return (
-    <div className={styles['post']}>
-      <Link className={styles['post__home-button']} to="/">All Courses</Link>
+    <div className="Post">
+      <Link className="Post-home-button" to="/">All Courses</Link>
 
-      <div className={styles['post__content']}>
+      <div className="Post-content">
         <Content body={html} title={title} />
       </div>
 
-      <div className={styles['post__footer']}>
+      <div className="Post-footer">
         <Tags tags={tags} tagSlugs={tagSlugs} />
         <Author />
       </div>
 
-      <div className={styles['post__comments']}>
+      <div className="Post-comments">
         <Comments postSlug={post.fields.slug} postTitle={post.frontmatter.title} />
       </div>
     </div>
