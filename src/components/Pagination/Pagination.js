2b03ea6d7908a525dc0f77ed75dabdf33da16732
Evan Cooper <evcooper@wayfair.com>
2019-05-03 06:56:58 -0400

    SCSS module conversion - Pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import classNames from 'classnames/bind';
-import { Link } from 'gatsby';
-import { PAGINATION } from '../../constants';
-import styles from './Pagination.module.scss';
-
-const cx = classNames.bind(styles);
+import React from "react";
+import { Link } from "gatsby";
+import { PAGINATION } from "../../constants";
+import "./Pagination.scss";
 
 const Pagination = ({
   prevPagePath,
@@ -12,24 +9,22 @@ const Pagination = ({
   hasNextPage,
   hasPrevPage
 }) => {
-  const prevClassName = cx({
-    'pagination__prev-link': true,
-    'pagination__prev-link--disable': !hasPrevPage
-  });
-
-  const nextClassName = cx({
-    'pagination__next-link': true,
-    'pagination__next-link--disable': !hasNextPage
-  });
-
   return (
-    <div className={styles['pagination']}>
-      <div className={styles['pagination__prev']}>
-        <Link rel="prev" to={prevPagePath} className={prevClassName}>{PAGINATION.PREV_PAGE}</Link>
-      </div>
-      <div className={styles['pagination__next']}>
-        <Link rel="next" to={nextPagePath} className={nextClassName}>{PAGINATION.NEXT_PAGE}</Link>
-      </div>
+    <div className="Pagination">
+      {hasPrevPage && (
+        <div className="Pagination-prev">
+          <Link rel="prev" to={prevPagePath} className="Pagination-prev-link">
+            {PAGINATION.PREV_PAGE}
+          </Link>
+        </div>
+      )}
+      {hasNextPage && (
+        <div className="Pagination-next">
+          <Link rel="next" to={nextPagePath} className="Pagination-next-link">
+            {PAGINATION.NEXT_PAGE}
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
