4f24403457000e654cb2a900ebdb3f92e0129988
Evan Cooper <evcooper@wayfair.com>
2019-05-03 06:44:18 -0400

    scss module conversion - Page

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from 'react';
-import styles from './Page.module.scss';
+import './Page.scss';
 
 const Page = ({ title, children }) => {
   const pageRef = useRef();
@@ -9,10 +9,10 @@ const Page = ({ title, children }) => {
   });
 
   return (
-    <div ref={pageRef} className={styles['page']}>
-      <div className={styles['page__inner']}>
-        { title && <h1 className={styles['page__title']}>{title}</h1>}
-        <div className={styles['page__body']}>
+    <div ref={pageRef} className="Page">
+      <div className="Page-inner">
+        { title && <h1 className="Page-title">{title}</h1>}
+        <div className="Page-body">
           {children}
         </div>
       </div>
